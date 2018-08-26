b14cd9311dc9d643ea693f97cf8cd6d586e0a8ca
RahifM <faizel326@gmail.com>
2018-08-26 23:44:25 +0000

    a5ultexx: AOSP-CAF Bringup
    Signed-off-by: RahifM <faizel326@gmail.com>

--- aosp_a5ultexx.mk
+++ aosp_a5ultexx.mk
@@ -15,11 +15,14 @@
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
+# include common configuration
+$(call inherit-product, vendor/aosp/common.mk)
+
 # Inherit from a5ultexx device
 $(call inherit-product, device/samsung/a5ultexx/device.mk)
 
 # Set those variables here to overwrite the inherited values.
-PRODUCT_NAME := lineage_a5ultexx
+PRODUCT_NAME := aosp_a5ultexx
 PRODUCT_DEVICE := a5ultexx
 PRODUCT_BRAND := samsung
 PRODUCT_MANUFACTURER := samsung
--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,1 +1,1 @@
-add_lunch_combo lineage_a5ultexx-userdebug
+add_lunch_combo aosp_a5ultexx-userdebug
